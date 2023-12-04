eb2a1dde8bbba50550e99ae66ae77aa82315e3b5
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-04 02:24:20 +0100

    add list mapper

---
+++ src/types/lists/mapper.ts
@@ -0,0 +1,11 @@
+import {WithId, WithoutId} from "mongodb";
+import {ListType, OutputListType} from "./output";
+
+export const listMapper= (input:WithId<ListType>):OutputListType=>{
+    return {
+        id: input._id.toString(),
+        userId:input.userId,
+        name:input.name,
+        items: input.items
+    }
+}
