c39bd04e7de064ff1d980580f19fce719fe1b0a3
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-09 03:05:30 +0100

    add userMapperWithHash

--- src/types/users/mapper.ts
+++ src/types/users/mapper.ts
@@ -1,4 +1,4 @@
-import {OutputUserType, UserType} from "./output";
+import {OutputUserType, UserType, UserTypeWithId} from "./output";
 import {WithId} from "mongodb";
 
 export const userMapper = (user: WithId<UserType>): OutputUserType => {
@@ -6,8 +6,19 @@ export const userMapper = (user: WithId<UserType>): OutputUserType => {
         id: user._id.toString(),
         name: user.name,
         login: user.login,
-        password: user.password,
         email: user.email,
+        lists: user.lists
+    }
+}
+
+export const userMapperWithHash = (user: WithId<UserType>): UserTypeWithId => {
+    return {
+        id: user._id.toString(),
+        name: user.name,
+        login: user.login,
+        email: user.email,
+        deleted:user.deleted,
+        hash:user.hash,
         lists: user.lists
     }
 }
