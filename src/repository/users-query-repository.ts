fb5560a4de08db2d8baf936aba6add86dcfd52aa
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-16 00:23:32 +0100

    _

--- src/repository/users-query-repository.ts
+++ src/repository/users-query-repository.ts
@@ -3,9 +3,23 @@ import {usersCollection} from "../db/db-collections";
 import {userMapper} from "../types/users/mapper";
 
 export class UsersQueryRepository{
+    static async getAllUsers(){
+        try{
+            const users = await usersCollection.find({}).toArray();
+            return users.map(userMapper);
+        }catch (err){
+            return null;
+        }
+
+
+    }
     static async getUserById(userId:string):Promise<OutputUserType|null>{
-        const user = await usersCollection.findOne({_id:new Object(userId)});
-        if (!user) return null
-        return userMapper(user)
+        try{
+            const user = await usersCollection.findOne({_id:new Object(userId)});
+            if (!user) return null
+            return userMapper(user)
+        }catch (err){
+            return null;
+        }
     }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import {listRouter} from "./routers/list-router";
+import {authRouter} from "./routers/auth-router";
+import {usersRouter} from "./routers/users-router";
 
 export const app = express()
 
-
+app.use("/auth", authRouter );
+app.use("/users", usersRouter);
 
 
