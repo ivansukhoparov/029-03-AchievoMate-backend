d37c0994cf7c3f1758146e0dcd6f36998f5658c7
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-16 02:54:26 +0100

    create get all users handler

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,20 +1,24 @@
 import express, {Request, Response} from "express";
 import {RequestWithBody} from "../types/common";
 import {CreateUserType} from "../types/users/input";
-import {UserType} from "../types/users/output";
 import {UsersService} from "../domains/users-service";
 import {HTTP_STATUSES} from "../utils/common";
+import {UsersQueryRepository} from "../repository/users-query-repository";
 
 
 export const usersRouter = express.Router();
 
-usersRouter.get("/",  (req:Request, res:Response)=>{
-    res.json({msg:"users"})
+usersRouter.get("/", async (req: Request, res: Response) => {
+    const users = await UsersQueryRepository.getAllUsers();
+    if (!users) {
+        res.sendStatus(500);
+        return
+    }
+    res.status(HTTP_STATUSES.OK_200).json(users);
 })
 
 usersRouter.post("/", async (req:RequestWithBody<CreateUserType>, res:Response)=>{
-
-    const newUser = await UsersService.createUser(req.body)
-    if (!newUser) res.status(HTTP_STATUSES.BAD_REQUEST_400)
-    else  res.status(HTTP_STATUSES.CREATED_201).json(newUser)
+    const newUser = await UsersService.createUser(req.body);
+    if (!newUser) res.status(HTTP_STATUSES.BAD_REQUEST_400);
+    else res.status(HTTP_STATUSES.CREATED_201).json(newUser);
 })
