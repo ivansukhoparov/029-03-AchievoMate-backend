71f3b808d0c51396bcb91606abdf1ec712c0b7fe
ivansukhoparov <rbcdaaa@gmail.com>
2023-12-16 03:45:21 +0100

    add errors type

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,5 +2,8 @@ import {Request} from "express";
 
 export type RequestWithBody<B> = Request<{},{},B,{}>;
 
-
-
+export type ErrorMessage = {
+    message: string
+    field: string
+}
+export type Errors = Array<ErrorMessage>
